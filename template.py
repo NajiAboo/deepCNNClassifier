6741d0e9c0886b2d647be6343ffaad202eec6db1
USER <naji.aboo@triassicsolutions.com>
2022-09-12 16:31:27 +0530

    seupt complete

--- template.py
+++ template.py
@@ -4,16 +4,20 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s: %(message)s]')
 
-package_nem = "deepClassifier"
+package_name = "deepClassifier"
 
 list_of_files =[
     ".github/workflows/.gitkeep",
-    f"src/{package_nem}/components/__init__.py",
-    f"src/{package_nem}/utils/__init__.py",
-    f"src/{package_nem}/config/__init__.py",
-    f"src/{package_nem}/pipeline/__init__.py",
-    f"src/{package_nem}/entity/__init__.py",
-    f"src/{package_nem}/constants/__init__.py",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/components/__init__.py",
+    f"src/{package_name}/utils/__init__.py",
+    f"src/{package_name}/config/__init__.py",
+    f"src/{package_name}/pipeline/__init__.py",
+    f"src/{package_name}/entity/__init__.py",
+    f"src/{package_name}/constants/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
     f"configs/config.yaml",
     "dvc.yaml",
     "parameter.yaml",
